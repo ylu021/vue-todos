b58d859531b1dac2cd9794de30fe36fe9131bb7e
ylu021 <yichen_0@hotmail.com>
2017-05-10 15:00:09 -0400

    using bash command to quickly commit, learning vue woohoo

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="app">
     <h1 v-text="title"></h1>
+    <input v-model="newItem" @keyup.enter="addNew">
     <ul v-for="item in list">
-      <li :class="{ finished: item.isFinished }">{{ item.label }}</li>
+      <li :class="{ finished: item.isFinished }" @click="toggleFinish(item)">
+        {{ item.label }}
+      </li>
     </ul>
-    <router-view></router-view>
+    <!-- <router-view></router-view> -->
   </div>
 </template>
 
@@ -15,19 +18,30 @@ export default {
     return {
       title: 'this is a todo list',
       list: [
-        {
-          label: 'eating',
-          isFinished: true
-        },
-        {
-          label: 'studying',
-          isFinished: false
-        },
-        {
-          label: 'practicing nodejs',
-          isFinished: false
-        }
-      ]
+        // {
+        //   label: 'eating',
+        //   isFinished: true
+        // },
+        // {
+        //   label: 'studying',
+        //   isFinished: false
+        // },
+        // {
+        //   label: 'practicing nodejs',
+        //   isFinished: false
+        // }
+      ],
+      newItem: ''
+    }
+  },
+  methods: {
+    toggleFinish: (item) => {
+      item.isFinished = !item.isFinished
+    },
+    addNew: function () {
+      console.log('enter pressed', this.newItem)
+      this.list.push({ label: this.newItem, isFinished: false })
+      this.newItem = ''
     }
   }
 }
@@ -35,7 +49,8 @@ export default {
 
 <style>
 .finished {
-  text-decoration: underline;
+  text-decoration: line-through;
+
 }
 
 #app {
