3c834b32802ca081854c4feaa2560406cd6116ca
ylu021 <yichen_0@hotmail.com>
2017-05-10 14:38:35 -0400

    using bash command to quickly commit, learning vue woohoo

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,17 +1,43 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <h1 v-text="title"></h1>
+    <ul v-for="item in list">
+      <li :class="{ finished: item.isFinished }">{{ item.label }}</li>
+    </ul>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data: () => {
+    return {
+      title: 'this is a todo list',
+      list: [
+        {
+          label: 'eating',
+          isFinished: true
+        },
+        {
+          label: 'studying',
+          isFinished: false
+        },
+        {
+          label: 'practicing nodejs',
+          isFinished: false
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <style>
+.finished {
+  text-decoration: underline;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
