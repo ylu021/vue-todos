ea48f29926593bd69d30841af222f1825ef2c391
ylu021 <yichen_0@hotmail.com>
2017-05-12 02:50:00 -0400

    remove everything to retry

--- todo/src/store.js
+++
@@ -1,7 +0,0 @@
-// const STORAGE_KEY = 'todo'
-export default {
-  fetch: (key) => {
-    return JSON.parse(localStorage.getItem(key))
-  },
-  save: (key, list) => (localStorage.setItem(key, JSON.stringify(list)))
-}
